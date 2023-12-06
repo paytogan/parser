9966e38df7c96d972af7d0aabd92c2b36f1e75af
SarahWa <123593148+paytogan@users.noreply.github.com>
2023-12-06 14:23:34 +0300

    Create iterItem.ts

---
+++ iterItem.ts
@@ -0,0 +1,24 @@
+import { Database, Parser, parseSingleItem } from "shared";
+
+const iterateItems = async (
+  database: Database,
+  isNew: boolean,
+  parseUrl: string,
+) => {
+  let i = 0;
+  for await (let doc of isNew
+    ? Parser.find({ isNew: true, status: "pending" })
+    : Parser.find({ status: "all" })) {
+    try {
+      if (doc.item === undefined) continue;
+      console.log(`[Update] -> Item #${i}`);
+      await parseSingleItem(doc, database, parseUrl, 10);
+    } catch (e) {
+      console.log("[Error] -> ", e);
+    }
+    i++;
+    //await sleep(200);
+  }
+};
+
+export default iterateItems;
